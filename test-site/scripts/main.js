95b46392ebe0d25d6d0afec60a92f1dac17b9134
vkurra <55491742+vkurra@users.noreply.github.com>
2019-09-18 17:18:52 +0530

    Add files via upload

---
+++ test-site/scripts/main.js
@@ -0,0 +1,54 @@
+/*let myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';*/
+
+/*let iceCream = 'chocolate';
+if(iceCream === 'chocolate') {
+  alert('Yay, I love chocolate ice cream!');    
+} else {
+  alert('Awwww, but chocolate is my favorite...');    
+}*/
+
+/*function multiply(num1,num2) {
+  let result = num1 * num2;
+  return result;
+}*/
+
+/*document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}*/
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute ('src','images/firefox2.png');
+    } else {
+      myImage.setAttribute ('src','images/firefox-icon.png');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName || myName === null) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
